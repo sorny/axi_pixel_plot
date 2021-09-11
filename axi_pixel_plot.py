0480f7d720f190fe146fb69104855433d67ba15b
sorny <sorny.at@gmail.com>
2021-09-11 09:56:51 +0200

    Refactor base script and add basic pytest

--- axi_pixel_plot.py
+++ axi_pixel_plot.py
@@ -10,6 +10,10 @@ import json
 import math
 import datetime
 
+# consts
+RESOLUTION = 0.05
+DURATION_FACTOR = 2.5
+
 
 # helper definitions
 def yes_or_no(question):
@@ -27,108 +31,129 @@ def write_recovery_file(data, file):
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
-def statistic(name, value):
-    return ' {0: <17}'.format(name + ': ') + value
-
-
-def print_statistics(args, image, resolution, axi_pen_downs, recovery_file, axi_pen_downs_recover):
-    duration_factor = 2.5
-    print('Plot statistics...')
-    print(statistic('Image', args.source_file.name))
-    print(statistic('Size', str(image.width) + 'x' + str(image.height) + 'px'))
-    print(statistic('Plot resolution', str(math.floor(1 / resolution)) + 'px/cm'))
-    print(
-        statistic('Plotsize', str(image.width / (1 / resolution)) + 'x' + str(image.height / (1 / resolution)) + 'cm'))
+def generate_plot_statistics(image, axi_pen_downs, axi_pen_downs_recover):
+    stats = dict()
+    stats['name'] = image.filename
+    stats['image_size'] = str(image.width) + 'x' + str(image.height) + 'px'
+    stats['plot_resolution'] = math.floor(1 / RESOLUTION)
+    stats['plot_size_width'] = image.width / (1 / RESOLUTION)
+    stats['plot_size_height'] = image.height / (1 / RESOLUTION)
+    stats['pen_downs'] = len(axi_pen_downs)
+    stats['plot_duration'] = stats['pen_downs'] / DURATION_FACTOR
+    stats['recover_pen_downs'] = stats['pen_downs']
+    stats['recover_plot_duration'] = stats['plot_duration']
     if len(axi_pen_downs_recover) > 0:
-        print(statistic('Recover file', recovery_file))
-        print(statistic('# Total Pen-Downs', str(len(axi_pen_downs))))
-        print(statistic('# Pen-Downs left', str(len(axi_pen_downs_recover))))
-        print(
-            statistic('Plot duration total', str(datetime.timedelta(seconds=(len(axi_pen_downs) / duration_factor)))))
-        print(statistic('Plot duration left',
-                        str(datetime.timedelta(seconds=(len(axi_pen_downs_recover) / duration_factor)))))
+        stats['recover_pen_downs'] = len(axi_pen_downs_recover)
+        stats['recover_plot_duration'] = ['recover_pen_downs'] / DURATION_FACTOR
+
+    return stats
+
+
+def print_statistic(name, value):
+    print(' {0: <17}'.format(name + ': ') + value)
+
+
+def print_statistics(image, axi_pen_downs, recovery_file, axi_pen_downs_recover):
+    stats = generate_plot_statistics(image, axi_pen_downs, axi_pen_downs_recover)
+    print('Plot statistics...')
+    statistic('Image', stats['name'])
+    statistic('Size', stats['image_size'])
+    statistic('Plot resolution', stats['image_size'])
+    statistic('Plotsize', str(stats['plot_size_width']) + 'x' + str(stats['plot_size_height']) + 'cm')
+
+    if len(axi_pen_downs_recover) > 0:
+        statistic('Recover file', recovery_file)
+        statistic('# Total Pen-Downs', str(stats['pen_downs']))
+        statistic('# Pen-Downs left', str(stats['recover_pen_downs']))
+        statistic('Plot duration total', str(datetime.timedelta(seconds=stats['plot_duration'])))
+        statistic('Plot duration left',
+                  str(datetime.timedelta(seconds=stats['recover_plot_duration'])))
     else:
-        print(statistic('# Pen-Downs', str(len(axi_pen_downs))))
-        print(statistic('Plot duration', str(datetime.timedelta(seconds=(len(axi_pen_downs) / duration_factor)))))
+        statistic('# Pen-Downs', str(stats['pen_downs']))
+        statistic('Plot duration', str(datetime.timedelta(seconds=stats['plot_duration'])))
     print()
 
 
-# cli args
-parser = argparse.ArgumentParser(description='Plot pixels on your AxiDraw')
-parser.add_argument('-p', '--plot', dest='action', action='store_const',
-                    const='plot', default='analyse', help='Plot the file')
-parser.add_argument('-a', '--analyse', dest='action', action='store_const',
-                    const='analyse', default='analyse', help='Analyse the file and print statistics')
-parser.add_argument('source_file', type=open,
-                    help='file to plot')
-args = parser.parse_args()
-
-### plotting business ###
-resolution = 0.05
-
-# load files
-image = Image.open(args.source_file.name)
-if image.mode != 'L':
-    raise Exception('Image mode ' + image.mode + ' not supported...')
-recovery_file = 'recovery_' + args.source_file.name.split('.')[0] + '.json'
-
-axi_pen_downs = []
-axi_pen_downs_recover = []
-recover = False
-if os.path.exists(recovery_file) and args.action == 'plot':
-    recover = yes_or_no('Continue plotting {} from {}?'.format(bmp_file, recovery_file))
-    if recover:
-        print('Recovering plotting data from {}...'.format(recovery_file))
-        axi_pen_downs_recover = json.loads(open(recovery_file, 'r').read())
-
-# analyse image
-print('Analysing {}...'.format(args.source_file.name))
-axipos_x = 0.0
-axipos_y = 0.0
-for x in range(image.width):
-    for y in range(image.height):
-        p = image.getpixel((x, y))
-        if p < 250:
-            axi_pen_downs.append((axipos_x, axipos_y))
-        axipos_y = round(axipos_y + resolution, 2)
+def generate_pen_downs(image):
+    pen_downs = []
+    axipos_x = 0.0
     axipos_y = 0.0
-    axipos_x = round(axipos_x + resolution, 2)
+    for x in range(image.width):
+        for y in range(image.height):
+            p = image.getpixel((x, y))
+            if p < 250:
+                pen_downs.append((axipos_x, axipos_y))
+            axipos_y = round(axipos_y + RESOLUTION, 2)
+        axipos_y = 0.0
+        axipos_x = round(axipos_x + RESOLUTION, 2)
+    return pen_downs
 
-print_statistics(args, image, resolution, axi_pen_downs, recovery_file, axi_pen_downs_recover)
 
-if args.action == 'plot':
-    print('Connecting to AxiDraw...')
-    ad = axidraw.AxiDraw()
-    ad.interactive()
-    connected = ad.connect()
-    if not connected:
-        sys.exit()
-    ad.options.units = 1
-    ad.update()
+if __name__ == '__main__':
+    # cli args
+    parser = argparse.ArgumentParser(description='Plot pixels on your AxiDraw')
+    parser.add_argument('-p', '--plot', dest='action', action='store_const',
+                        const='plot', default='analyse', help='Plot the file')
+    parser.add_argument('-a', '--analyse', dest='action', action='store_const',
+                        const='analyse', default='analyse', help='Analyse the file and print statistics')
+    parser.add_argument('source_file', type=open,
+                        help='file to plot')
+    args = parser.parse_args()
 
-    if not yes_or_no('Start plotting?'):
-        sys.exit()
-    print('Plotting...')
-    if recover:
-        axi_pen_downs = axi_pen_downs_recover
-    unplotted = [i for i in axi_pen_downs]
+    # load files
+    image = Image.open(args.source_file.name)
+    if image.mode != 'L':
+        raise Exception('Image mode ' + image.mode + ' not supported...')
+    recovery_file = 'recovery_' + args.source_file.name.split('.')[0] + '.json'
 
-    try:
-        plotting_progress = tqdm(axi_pen_downs, desc='Plotting progress', unit='pd')
-        for pd in plotting_progress:
-            plotting_progress.set_description('Plotting progress ({:.2f}/{:.2f})'.format(pd[0], pd[1]), refresh=True)
-            ad.goto(pd[0], pd[1])
-            ad.pendown()
-            ad.penup()
-            unplotted.remove(pd)
+    axi_pen_downs = []
+    axi_pen_downs_recover = []
+    recover = False
+    if os.path.exists(recovery_file) and args.action == 'plot':
+        recover = yes_or_no('Continue plotting {} from {}?'.format(bmp_file, recovery_file))
+        if recover:
+            print('Recovering plotting data from {}...'.format(recovery_file))
+            axi_pen_downs_recover = json.loads(open(recovery_file, 'r').read())
 
-        print('Done, returning to home...')
-        # hack to also write recovery file if pause button is pressed on AxiDraw
-        if len(unplotted) > 0:
+    # analyse image
+    print('Analysing {}...'.format(image.name))
+    axi_pen_downs = generate_pen_downs(image)
+    print_statistics(args, image, axi_pen_downs, recovery_file, axi_pen_downs_recover)
+
+    if args.action == 'plot':
+        print('Connecting to AxiDraw...')
+        ad = axidraw.AxiDraw()
+        ad.interactive()
+        connected = ad.connect()
+        if not connected:
+            sys.exit()
+        ad.options.units = 1
+        ad.update()
+
+        if not yes_or_no('Start plotting?'):
+            sys.exit()
+        print('Plotting...')
+        if recover:
+            axi_pen_downs = axi_pen_downs_recover
+        unplotted = [i for i in axi_pen_downs]
+
+        try:
+            plotting_progress = tqdm(axi_pen_downs, desc='Plotting progress', unit='pd')
+            for pd in plotting_progress:
+                plotting_progress.set_description('Plotting progress ({:.2f}/{:.2f})'.format(pd[0], pd[1]),
+                                                  refresh=True)
+                ad.goto(pd[0], pd[1])
+                ad.pendown()
+                ad.penup()
+                unplotted.remove(pd)
+
+            print('Done, returning to home...')
+            # hack to also write recovery file if pause button is pressed on AxiDraw
+            if len(unplotted) > 0:
+                write_recovery_file(unplotted, recovery_file)
+        except KeyboardInterrupt:
             write_recovery_file(unplotted, recovery_file)
-    except KeyboardInterrupt:
-        write_recovery_file(unplotted, recovery_file)
 
-    finally:
-        ad.goto(0, 0)
-        ad.disconnect()
+        finally:
+            ad.goto(0, 0)
+            ad.disconnect()
---
+++ test_axi_pixel_plot.py
@@ -0,0 +1,22 @@
+import pytest
+from PIL import Image
+import axi_pixel_plot
+
+
+def test_statistics_bmp():
+    image = Image.open('test.bmp')
+    assert image.mode == 'L'
+
+    pen_downs = axi_pixel_plot.generate_pen_downs(image)
+    assert len(pen_downs) == 400
+
+    stats = axi_pixel_plot.generate_plot_statistics(image, pen_downs, [])
+    assert stats['name'] == 'test.bmp'
+    assert stats['image_size'] == '20x20px'
+    assert stats['plot_size_width'] == 1
+    assert stats['plot_size_height'] == 1
+    assert stats['pen_downs'] == 400
+    assert stats['plot_duration'] == 160
+    assert stats['recover_pen_downs'] == stats['pen_downs']
+    assert stats['recover_plot_duration'] == stats['plot_duration']
+    assert stats['name'] == 'test.bmp'
